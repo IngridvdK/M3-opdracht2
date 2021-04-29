9fa244c5a523f223491615dcc9e197a9086a2ba2
IngridvdK <79993280+IngridvdK@users.noreply.github.com>
2021-04-29 20:17:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hallo Winc academy studenten")
+// Dit is een grote som. En is onzichtbaar bij het runnen van de code.
+// En dit /* */ maakt alle sommen onzichtbaar.
+/*let multiplication = 1230941 * 1823794;
+console.log("multiplication:", multiplication);
+let substration = 637263 - 54;
+console.log("substration:", substration);*/
